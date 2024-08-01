34a7a8942b56b6387080cbf2a43f71f42d36ad9b
Sharifarezaie <Sharifashaida82@gmail.com>
2024-08-01 19:08:44 +0430

    fedback issue

--- script.js
+++ script.js
@@ -7,17 +7,17 @@
 // }
 
 // Toggle between showing and hiding the sidebar when clicking the menu icon
-// let mySidebar = document.getElementById("mySidebar");
+let mySidebar = document.getElementById("mySidebar");
 
-// function w3_open() {
-//   if (mySidebar.style.display === 'block') {
-//     mySidebar.style.display = 'none';
-//   } else {
-//     mySidebar.style.display = 'block';
-//   }
-// }
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
 
 // Close the sidebar with the close button
-// function w3_close() {
-//   mySidebar.style.display = "none";
-// }
+function w3_close() {
+  mySidebar.style.display = "none";
+}
